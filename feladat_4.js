d547c07a2340672fc2f359a4d2ea7715c98f2160
Daniel-bits <d.dobrentey@protonmail.com>
2022-03-02 19:19:46 +0100

    feladat4

--- feladat_4.js
+++ feladat_4.js
@@ -1,7 +1,11 @@
 /* Write a function checkSpam(str) that returns true if str contains ‘viagra’ or ‘XXX’, otherwise false. The function must be case-insensitive. */
 
+let checkSpam = function(str) {
+str = str.toLowerCase();
+const result = (str.includes('viagra') || str.includes('xxx')) ? true : false;
+return result;
+}
 
-
-checkSpam('buy ViAgRA now') == true
-checkSpam('free xxxxx') == true
-checkSpam("innocent rabbit") == false
+console.log(checkSpam('buy ViAgRA now') == true);
+console.log(checkSpam('free xxxxx') == true);
+console.log(checkSpam("innocent rabbit") == false);
